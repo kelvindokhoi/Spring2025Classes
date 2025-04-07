58bbe8df68ca0733409e8acbbe9ae0c3e3de53bf
Nguyen Do <konu0315@gmail.com>
2025-04-07 15:31:51 -0400

    Changes to be committed: 	modified:   DataStructure_CSC225/avl_tree.py 	new file:   DataStructure_CSC225/avl_tree_out.txt

--- DataStructure_CSC225/avl_tree.py
+++ DataStructure_CSC225/avl_tree.py
@@ -1,4 +1,5 @@
-# avl_tree
+# magic_tree
+# python avl_tree.py > avl_tree_out.txt
 
 class Node:
     def __init__(self,val):
@@ -8,6 +9,8 @@ class Node:
         self.val = val
         self.rb = 0
         self.lb = 0
+    def __str__(self):
+        return f"{self.val} | rb = {self.rb}, lb = {self.lb}, p = {None if self.parent is None else self.parent.val}"
 
 class AVL_Tree:
     def __init__(self):
@@ -18,82 +21,178 @@ class AVL_Tree:
             self.root = newnode
             return
         # checked the root is empty
-        if self.root.height == 1 and self.root.left is None and self.root.val>=value:
-            newnode.parent = self.root
-            self.root.left = newnode
-            self.root.lb +=1
-            return
-        if self.root.height == 1 and self.root.right is None and self.root.val<value:
+        if self.root.right is None and value>self.root.val:
             newnode.parent = self.root
             self.root.right = newnode
-            self.root.rb +=1
-            return  
-        # checked if there are only 2 or less than 2 nodes
+            self.root.rb = 1
+            return
+        if self.root.left is None and value<=self.root.val:
+            newnode.parent = self.root
+            self.root.left = newnode
+            self.root.lb = 1
+            return
+        #check is there is a place to put under the root
         p = self.root
+        parent = None
         while p is not None:
+            parent = p
             if value > p.val:
                 p = p.right
             else:
                 p = p.left
-        p = p.parent
         # found the place to insert
-        newnode.parent = p
-        if value>p.val:
-            p.right = newnode
-            p.rb +=1
+        if value>parent.val:
+            parent.right = newnode
         else:
-            p.left = newnode
-            p.lb +=1
+            parent.left = newnode
+        newnode.parent = parent
         # inserted
-        follow = p.val
-        p = p.parent
-        while p is not None:
-            if p.val<follow:
-                p.rb+=1
+        print(f'({newnode}) is added to ({parent})')
+        print('Balancing...')
+        self.balancetree(newnode)
+    def balancetree(self,node):
+        # adding lb and rb to the tree
+        while node is not None:
+            print(f'Currently at: {node}')
+            parent = node.parent
+            if parent is None:
+                break
+            if node.val>parent.val:
+                parent.rb += 1
             else:
-                p.lb-=1
-            #-check for imbalance
-            #if imba, rotate
-            balance = p.rb - p.lb
-            if abs(balance)<2:
-                pass
+                parent.lb += 1
+            # finished incrementing
+            balance = parent.rb - parent.lb
+            # trying the RR and RL
+            if balance == 2:
+                if 1>=node.rb-node.lb>=0:
+                    self.RR(parent)
+                else:
+                    self.RL(parent)
+                return
+            # trying the LL and LR
+            if balance == -2:
+                if -2<node.rb-node.lb<1:
+                    self.LL(parent)
+                else:
+                    self.LR(parent)
+                return
+            node = parent
+    def RR(self,node):
+        print('RR-ing node', node)
+        A = node
+        B = node.right
+        T2 = B.left
+        print(f'A = {A}')
+        print(f'B = {B}')
+        print(f'T2 = {T2}')
+        p = A.parent
+        if p is None:
+            self.root = B
+            B.parent = None
+        else:
+            if B.val > p.val:
+                p.right = B
+                B.parent = p
             else:
-                # check for RR
-                if balance == 2:
-                    if p.right.rb-p.right.lb>-1:
-                        A = p
-                        B = p.right
-                        T2 = p.right.left
-                        if A.parent is None:
-                            # A is the ROOT
-                            A.right = T2
-                            T2.parent = A
-                            A.parent = B
-                            B.left = A
-                            self.root = B
-                        else:
-                            # A is attached to something
-                            A.right = T2
-                            T2.parent = A
-                            A.parent = B
-                            B.left = A
-                            B.parent = p.parent
-                            if p.parent.val < p.val:
-                                p.parent.right = B
-                            else:
-                                p.parent.left = B
-                        return
-                    else:
-                        ...
-                    # check for RL
-                # check for LL
-                elif balance ==-2:
-                    if p.left.rb-p.left.lb<1:
-                        ...
-                    else:
-                        ...
-                    # check for LR
-            follow = p.val
-            p = p.parent
-        # increment or decrement the whole parents
+                p.left = B
+                B.parent = p
+        if B.rb - B.lb == 1:
+            A.rb = A.rb - 2
+        else:
+            A.rb = A.rb - 1
+        B.rb -= 1
+        A.parent = B
+        A.right = T2
+        if T2 is not None:
+            T2.parent = A
+        B.left = A
+        print('After RR rotation:')
+        print(f'Root: {self.root}')
+        print(f'Right {self.root.right}')
+        print(f'Left: {self.root.left}')
+    def LL(self,node):
+        print('LL-ing node',node)
+        A = node
+        B = node.left
+        T2 = B.right
+        print(f'A = {A}')
+        print(f'B = {B}')
+        print(f'T2 = {T2}')
+        p = A.parent
+        if p is None:
+            self.root = B
+            B.parent = None
+        else:
+            if B.val > p.val:
+                p.right = B
+                B.parent = p
+            else:
+                p.left = B
+                B.parent = p
+        if B.rb - B.lb == -1:
+            A.lb -= 2
+        else:
+            A.lb -= 1
+        B.lb -= 1
+        A.parent = B
+        A.left = T2
+        if T2 is not None:
+            T2.parent = A
+        B.right = A
+    def RL(self,node):
+        print('RL ROTATION')
+        self.LL(node.right)
+        self.RR(node)
+
+    def LR(self,node):
+        print('LR ROTATION')
+        self.RR(node.left)
+        self.LL(node)
+
+    def inorder(self):
+        self.inorderPrint(self.root)
+        print()
+    
+    def postorder(self):
+        self.postorderPrint(self.root)
+        print()
+
+    def preorder(self):
+        self.preorderPrint(self.root)
+        print()
+
+    def inorderPrint(self,currnode):
+        if currnode is None:
+            return
+        self.inorderPrint(currnode.left)
+        print(currnode.val,end=' ')
+        self.inorderPrint(currnode.right)
+    
+    def preorderPrint(self,currnode):
+        if currnode is None:
+            return
+        print(currnode.val,end=' ')
+        self.preorderPrint(currnode.left)
+        self.preorderPrint(currnode.right)
+    
+    def postorderPrint(self,currnode):
+        if currnode is None:
+            return
+        self.postorderPrint(currnode.left)
+        self.postorderPrint(currnode.right)
+        print(currnode.val,end=' ')
+    def delete(self,value):
+        ...
+
+nums = [2,0,1]
+mytree = AVL_Tree()
+mytree.inorder()
+for i in nums:
+    print(f'Adding {i}.....')
+    mytree.add(i)
+    mytree.inorder() #LSR
+    mytree.preorder() #SLR
+    mytree.postorder() #LRS
+    print('----------------------------------------------')
 
